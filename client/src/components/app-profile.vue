95876bb91c33f2dd2da598d929bf17dd2694ebb6
wastech <fataiwasiu2@gmail.com>
2021-11-17 22:48:34 +0100

    worked more on homepage

---
+++ client/src/components/app-profile.vue
@@ -0,0 +1,106 @@
+<template>
+  <div>
+    <div class="row q-col-gutter-xs">
+      <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 col-xl-4">
+        <div class="title q-my-md">
+          <div class="text-h5">About Me</div>
+        </div>
+        <div class="image">
+          <img
+            src="https://awcdn1.ahmad.works/writing/wp-content/uploads/2015/05/Author.jpg"
+            alt=""
+          />
+        </div>
+        <div class="text q-my-sm">
+          <div class="text-body2">
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+            erat, sed diam voluptua.
+          </div>
+        </div>
+      </div>
+      <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 tweet__section">
+        <div class="title q-my-md">
+          <div class="text-h5">Tweets</div>
+        </div>
+        <div class="tweet q-my-md">
+          <div class="text-body1">
+            <q-icon name="fab fa-twitter" size="xs" class="q-mr-sm"></q-icon>
+            <span class="text-bold"
+              ><a href="/" class="a__about">Ahmad Works</a>
+            </span>
+            @AppleSupport If you need to know the version number, its 9.2.1
+            (13D15)
+          </div>
+
+          <div class="duration">
+            <div class="text-caption">
+              <a href="/" class="a">about 2 months ago</a>
+            </div>
+          </div>
+          <hr style="color: #ccc" />
+        </div>
+      </div>
+      <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 col-xl-4">
+        <div class="title q-my-md">
+          <div class="text-h5">Instagram</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.text-h5 {
+  font-size: 15px;
+  font-weight: bold;
+  color: #000;
+}
+img {
+  border-radius: 15px;
+  object-fit: cover;
+  height: auto;
+}
+.body2 {
+  font-size: 14px;
+  line-height: 23px;
+  color: #666;
+}
+.body1 {
+  line-height: 20px;
+  font-size: 13px;
+  color: #222;
+}
+
+.fa-twitter {
+  color: #32aadc;
+}
+.a__about {
+  text-decoration: none;
+  font-weight: 700;
+  color: #333;
+}
+.a__about:hover {
+  text-decoration: underline;
+  font-weight: 700;
+  color: #333;
+}
+.a {
+  clear: both;
+  display: inline-block;
+  font-size: 11px;
+  color: #8899a6;
+  text-decoration: none;
+}
+.a:hover {
+  clear: both;
+  text-decoration: underline;
+  display: inline-block;
+  font-size: 11px;
+  color: #8899a6;
+}
+</style>
--- client/src/pages/Index.vue
+++ client/src/pages/Index.vue
@@ -2,18 +2,30 @@
   <div class="main">
     <app-header />
     <app-item />
+    <div class="q-pa-lg flex flex-center">
+      <q-pagination
+        v-model="current"
+        color="deep-orange"
+        :max="5"
+        boundary-links
+      />
+    </div>
+    <app-profile />
   </div>
 </template>
 
 <script>
 import appHeader from "../components/app-header.vue";
 import AppItem from "../components/app-item.vue";
+import AppProfile from "../components/app-profile.vue";
 
 export default {
-  components: { appHeader, AppItem },
+  components: { appHeader, AppItem, AppProfile },
   name: "PageIndex",
   data() {
-    return {};
+    return {
+      current: 3,
+    };
   },
 };
 </script>
