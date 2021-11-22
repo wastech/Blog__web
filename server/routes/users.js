61de5743d93738707fb823b8290bb8b376898272
wastech <fataiwasiu2@gmail.com>
2021-11-23 00:09:00 +0100

    :joy:

--- server/routes/users.js
+++ server/routes/users.js
@@ -14,8 +14,8 @@ const router = express.Router({ mergeParams: true });
 const advancedResults = require("../middleware/advancedResults");
 const { protect, authorize } = require("../middleware/auth");
 
-// router.use(protect);
-// router.use(authorize("admin"));
+router.use(protect);
+router.use(authorize("admin"));
 
 router.route("/").get(advancedResults(User), getUsers).post(createUser);
 
